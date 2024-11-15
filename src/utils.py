9354ad5c2bd3614fdc71db66458b9046dec6301a
Anderson Souza <andluiz@ifi.unicamp.br>
2024-11-15 10:29:49 -0300

    feat: :sparkles: Create setup and completion to invoke models

---
+++ src/utils.py
@@ -0,0 +1,23 @@
+import os
+import logging
+import google.generativeai as genai
+from dotenv import load_dotenv
+
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+
+def setup_google_credentials():
+    
+    load_dotenv(".env")
+    GOOGLE_API_KEY = os.environ["GOOGLE_API_KEY"]
+    genai.configure(api_key=GOOGLE_API_KEY)
+
+
+def get_completion(prompt, model="gemini-1.5-flash"):
+
+    llm = ChatGoogleGenerativeAI(
+        model=model, temperature=0.0, max_output_tokens=1024
+    )
+    response = llm.invoke(prompt)
+
+    return response.content
